5db0a34144dc2eba3c17fab5d605903b90c92be8
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-10 09:20:41 -0500

    Update gui_scrap.py

--- gui_scrap.py
+++ gui_scrap.py
@@ -1,46 +1,30 @@
 """
-
-Drudge Report Scrapper w/ nice GUI
-The hope is to import the module that does the actual scrapping
-
+PySimpleGui program that displays web content from beautifulsoup/requests. 
+TODO:
+    - text format/color/size
+    - import the scrapping module
+    - errors along the way
 """
+
 #!/usr/bin/env python
-#IMPORT DRUDGE-SCRAP module
 import PySimpleGUI as sg
 import requests
 from bs4 import BeautifulSoup
-import sys
-import time
-import faker
-from faker import Faker
 
 
-def fakeInfo():
-    fake = Faker()
-    names = []
-    display_names = []
-    
-    for x in range(20):
-        names.append(fake.name())
-    
-    for i in names:
-        j = i.replace(' ',')                                                                                ')
-        display_names.append(j)
-
-    return display_names
-
-""" Scraps drudge, needs to be displayed better though """
+""" Scraps a website, needs to be displayed better though """
 def drudgeGuy():
-    url = 'http://drudgereport.com'
-    source_code = requests.get(url) # pass the url to requests, HTTP for humans
+    url = ''    #enter website-url here
+    source_code = requests.get(url)
     plain_text = source_code.text 
-    soup = BeautifulSoup(plain_text, "html.parser") # pass/parse the url with bs4
+    soup = BeautifulSoup(plain_text, "html.parser") 
     if soup:
         for column in soup.find_all('a'):
             print("SCRAPING %s" % url)
             return soup.get_text()
     else:
         return
+    
 
 """ Currently not used, but easy to implement if need be """
 def SecondForm():
@@ -56,20 +40,14 @@ def TestMenus():
     sg.ChangeLookAndFeel('GreenMono')
     sg.SetOptions(element_padding=(10, 0))
 
-    fkmod = Faker()
-
-    # ------ GUI Defintion ------ #
     layout = [
-               [sg.PopupScrolled("JDG", fkmod.name(), drudgeGuy())],
+               [sg.PopupScrolled("JDG", drudgeGuy())],
                [sg.In('JDG', key='input', do_not_clear=False)],
                [sg.OK(button_color=('black', 'red'))]
              ]
 
     window = sg.Window("JDG Drude Feed", default_element_size=(12, 1), auto_size_text=False, auto_size_buttons=False, auto_close_duration = 2).Layout(layout)
-
-    # ------ Start the GUI ------ #
     window.Read()
-
 
 
 
