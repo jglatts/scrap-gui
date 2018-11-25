b10877bda72b9255662eda1a809c5c39916c3f9a
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-11-25 17:39:05 -0500

    Update buttonGUIscrap.py

--- buttonGUIscrap.py
+++ buttonGUIscrap.py
@@ -1,3 +1,8 @@
+"""
+
+- Refactor Mane!!! Very cluttered at the moments
+
+"""
 import PySimpleGUI as sg
 import requests
 import string
@@ -6,14 +11,14 @@ from faker import Faker
 
 
 class GetSoup(object):
-    """ Scraps a website, needs to be displayed better though """
+    """ Scraps a website for the GUI to display. A function call may be easier, but we out here learning OOP. """
 
     def __init__(self, url):
         super(GetSoup, self).__init__()
         self.url = url
 
-    """ add **args here to search for different elements """
-    """ something like a flag that will change the bs4 scrap """
+    """ determine how bs4 will search the provided urls.  """
+    """ Find the more effective way. """
     def scrap(self, quick_search=True):
         user_url = self.url
         source_code = requests.get(user_url)
@@ -22,16 +27,18 @@ class GetSoup(object):
         if soup:
             # logical error here, prints the wrong url
             print("SCRAPING %s" % user_url)
+            # change the tags that bs4 will search for, find more effective way of doing so
             if quick_search:
+                # drudge-gui, so this will have 3 columns of content
                 for column in soup.find_all("td"):
-                    print(column.get_text())
                     return column.get_text()
             else:
+                # not finding all elements
                 for column in soup.find_all("a"):
-                    print(column.get_text())
                     return column.get_text()
         else:
             return
+
 
 
 def begin_form():
@@ -41,16 +48,16 @@ def begin_form():
     layout = [
                 [sg.Text('Scrap Feed!', size=(21, 1), justification='center', font=("Helvetica", 35), text_color="blue",
                  relief=sg.RELIEF_RIDGE)],
-                [sg.Button('Site1-GUI', button_color=('black', 'red'), font=("Helvetica", 35)),
-                 sg.Button('Site2-GUI', font=("Helvetica", 35))]
+                [sg.Button('Drudge-GUI', button_color=('black', 'red'), font=("Helvetica", 35)),
+                 sg.Button('Google-GUI', font=("Helvetica", 35))]
              ]
 
     window = sg.Window('Whadup Mane').Layout(layout)
     while True:
         button, value = window.Read()
-        if button == 'Site1-GUI':
+        if button == 'Drudge-GUI':
             test_menus()
-        elif button == 'Site2-GUI':
+        elif button == 'Google-GUI':
             test_menus(False)
         else:
             return
@@ -60,8 +67,8 @@ def test_menus(check=True):
     """ Display information """
 
     # add website-urls here
-    url_one = GetSoup("").scrap()
-    url_two = GetSoup("").scrap(False)
+    url_one = GetSoup("https://www.drudgereport.com/").scrap()
+    url_two = GetSoup("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en").scrap(False)
 
     # helper function on the way to get rid of repetition
     output = url_one.strip()
@@ -77,19 +84,19 @@ def test_menus(check=True):
             filtered_two += char_two
 
     sg.ChangeLookAndFeel('GreenMono')
-    sg.SetOptions(element_padding=(8, 0))
+    sg.SetOptions(element_padding=(5, 0))
 
     if check:
         layout = [
-            [sg.Text('Web Feed!', size=(45, 1), justification='center', font=("Helvetica", 35), text_color="blue",
+            [sg.Text('Drudge Report', size=(20, 1), justification='center', font=("Helvetica", 35), text_color="blue",
                      relief=sg.RELIEF_RIDGE)],
-            [sg.Multiline(filtered_one, size=(80, 40))],
-            # sg.Multiline(filtered_two, size=(80, 40))],
+            [sg.Multiline(filtered_one, size=(70, 12))],
+            # sg.Multiline(filtered_two, size=(80, 20))],
             [sg.In('JDG', key='input', do_not_clear=True)],
             [sg.Button('HOME', button_color=('black', 'red')), sg.Button('EXIT')]
         ]
 
-        window = sg.Window("Web Feed", default_element_size=(12, 1), auto_size_text=False, auto_size_buttons=True).Layout(layout)
+        window = sg.Window("WEAS Feed", default_element_size=(12, 1), auto_size_text=False, auto_size_buttons=True).Layout(layout)
         # read values from buttons and respond accordingly
         while True:
             event, value = window.Read()
@@ -99,10 +106,10 @@ def test_menus(check=True):
                 return
     else:
         layout = [
-            [sg.Text('Web Feed!', size=(45, 1), justification='center', font=("Helvetica", 35), text_color="blue",
+            [sg.Text('Google-News', size=(20, 1), justification='center', font=("Helvetica", 35), text_color="blue",
                      relief=sg.RELIEF_RIDGE)],
-            #[sg.Multiline(filtered_one, size=(80, 40)),
-            [sg.Multiline(filtered_two, size=(80, 40))],
+            #[sg.Multiline(filtered_one, size=(80, 20)),
+            [sg.Multiline(filtered_two, size=(70, 12))],
             [sg.In('JDG', key='input', do_not_clear=True)],
             [sg.Button('HOME', button_color=('black', 'red')), sg.Button('EXIT')]
         ]
@@ -116,5 +123,6 @@ def test_menus(check=True):
                 begin_form()
             else:
                 return
+
 
 begin_form()
