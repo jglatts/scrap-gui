eefe0f7a7d57e49b9a540e83926c5d21a16b3f03
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-12-09 09:08:30 -0500

    Update buttonGUIscrap.py

--- buttonGUIscrap.py
+++ buttonGUIscrap.py
@@ -2,45 +2,80 @@ import PySimpleGUI as sg
 import requests
 import string
 from bs4 import BeautifulSoup
-from faker import Faker
 
 
 def bayscrap(products):
-    """ Get some info from EBAY with requests and bs4. """
-    """ Add empty scrap check """
+    """ Get some info from EBAY with requests and bs4. 
+        Add empty scrap check 
+    """
+    
     url = 'https://www.ebay.com/sch/' + products
     # Add headers
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    source_code = requests.get(url, headers=headers)  # pass the url to requests, HTTP for humans
+    source_code = requests.get(url, headers=headers)  
     plain_text = source_code.text
 
-    content = []
-    soup = BeautifulSoup(plain_text, "html.parser")  # pass/parse the url with bs4
+    content = ''
+    soup = BeautifulSoup(plain_text, "html.parser")  
     for items in soup.find_all("div", {"class": "s-item__info clearfix"}):
-        print('\n' + items.get_text())
-        content.append(items.get_text())
+        # work on filtering
+        for char in items.get_text():
+            if char in string.printable:
+                content += char
+        content += '\n\n\n'
 
     return content
 
 
-
-def soldlistings(products):
+def sold_menu(products):
     """ Scrap EBAY for sold items of the search product.
     This changes the url to find the sold listings.
-    New URL will be fed to requests\bs4.  """
+    New URL will be fed to requests\bs4.
+    BREAK THIS UP MAYNE
+    """
 
     print('\nDisplaying Sold Items For:' + ' ' + products.title() + '\n')
     sold_url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=' + products + '&_sacat=0&LH_Sold=1&_dmd=2'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    src_sold = requests.get(sold_url, headers=headers)  # pass the url to requests, HTTP for humans
+    src_sold = requests.get(sold_url, headers=headers)
     bs4_text = src_sold.text
-
-    # Throw findings to bs4 then display
     sold_soup = BeautifulSoup(bs4_text, 'html.parser')
+    
+    filtered = ''
     for sold in sold_soup.find_all("li", {"class": "s-item"}):
         print('\n' + sold.get_text())
+        # filtering
+        for char in sold.get_text():
+            if char in string.printable:
+                filtered += char
+        filtered += '\n\n\n'
+
+    print(filtered)
+    sg.ChangeLookAndFeel('GreenMono')
+    sg.SetOptions(element_padding=(5, 0))
+    heading = ("sold %s-products" % products)
+
+    layout = [
+            [sg.Text(heading.title(), size=(20, 1), justification='center', font=("Helvetica", 35), text_color="blue",
+                relief=sg.RELIEF_RIDGE)],
+            [sg.Multiline(filtered, size=(70, 12))],
+            [sg.In('JDG', key='input', do_not_clear=True)],
+            [sg.Button('HOME', button_color=('black', 'white')), sg.Button('Sold-Listings', button_color=('black', 'white')), sg.Button('EXIT', button_color=('black', 'white'))]
+        ]
+
+    window = sg.Window("WEAS Feed", default_element_size=(12, 1), auto_size_text=False,
+                           auto_size_buttons=True).Layout(layout)
+    # read values from buttons and respond accordingly
+    while True:
+        event, value = window.Read()
+        if event == 'HOME':
+            begin_form()
+        elif event == 'Sold-Listings':
+            sold_menu(product)
+        else:
+            return
 
 
 def begin_form():
@@ -67,15 +102,17 @@ def begin_form():
         else:
             return
 
+
 def test_menus(product):
     """ Display information """
 
     output = bayscrap(product)
+    # filtering
     filtered = ''
     for char_two in output:
         if char_two in string.printable:
             filtered += char_two
-
+    print(filtered)
     sg.ChangeLookAndFeel('GreenMono')
     sg.SetOptions(element_padding=(5, 0))
     heading = ("%s-products" % product)
@@ -85,8 +122,9 @@ def test_menus(product):
                 relief=sg.RELIEF_RIDGE)],
             [sg.Multiline(output, size=(70, 12))],
             [sg.In('JDG', key='input', do_not_clear=True)],
-            [sg.Button('HOME', button_color=('black', 'red')), sg.Button('EXIT')]
-        ]
+        [sg.Button('HOME', button_color=('black', 'white')),
+         sg.Button('Sold-Listings', button_color=('black', 'white')), sg.Button('EXIT', button_color=('black', 'white'))]
+    ]
 
     window = sg.Window("WEAS Feed", default_element_size=(12, 1), auto_size_text=False,
                            auto_size_buttons=True).Layout(layout)
@@ -94,10 +132,11 @@ def test_menus(product):
     while True:
         event, value = window.Read()
         if event == 'HOME':
-            pass
-        elif event == 'Exit':
-            return
+            begin_form()
+        elif event == 'Sold-Listings':
+            sold_menu(product)
         else:
             return
+
 
 begin_form()
