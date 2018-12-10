5c01acfc3a31a4e98ec758712df9d9ef5878d8f3
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-12-10 00:23:06 -0500

    Update buttonGUIscrap.py

--- buttonGUIscrap.py
+++ buttonGUIscrap.py
@@ -1,3 +1,32 @@
+"""
+
+__/\\\\\\\\\\\\\\\__/\\\___________________________________________________/\\\\\\\\\\\____________________________________________________________
+ _\/\\\///////////__\/\\\_________________________________________________/\\\/////////\\\__________________________________________________________
+  _\/\\\_____________\/\\\__________________________/\\\__/\\\____________\//\\\______\///_______________________________________________/\\\\\\\\\__
+   _\/\\\\\\\\\\\_____\/\\\_________/\\\\\\\\\______\//\\\/\\\______________\////\\\_____________/\\\\\\\\__/\\/\\\\\\\___/\\\\\\\\\_____/\\\/////\\\_
+    _\/\\\///////______\/\\\\\\\\\__\////////\\\______\//\\\\\__________________\////\\\________/\\\//////__\/\\\/////\\\_\////////\\\___\/\\\\\\\\\\__
+     _\/\\\_____________\/\\\////\\\___/\\\\\\\\\\______\//\\\______________________\////\\\____/\\\_________\/\\\___\///____/\\\\\\\\\\__\/\\\//////___
+      _\/\\\_____________\/\\\__\/\\\__/\\\/////\\\___/\\_/\\\________________/\\\______\//\\\__\//\\\________\/\\\__________/\\\/////\\\__\/\\\_________
+       _\/\\\\\\\\\\\\\\\_\/\\\\\\\\\__\//\\\\\\\\/\\_\//\\\\/________________\///\\\\\\\\\\\/____\///\\\\\\\\_\/\\\_________\//\\\\\\\\/\\_\/\\\_________
+        _\///////////////__\/////////____\////////\//___\////____________________\///////////________\////////__\///___________\////////\//__\///__________
+
+    - YERRRRP
+    - Copyright 2018 John Glatts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+    rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+    persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and
+    this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED
+    "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+
+
 import PySimpleGUI as sg
 import requests
 import string
@@ -6,51 +35,49 @@ from bs4 import BeautifulSoup
 
 def bayscrap(products):
     """ Get some info from EBAY with requests and bs4.
-        Add empty scrap check 
+        Add empty scrap check
     """
+
     url = 'https://www.ebay.com/sch/' + products
     # Add headers
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    source_code = requests.get(url, headers=headers)  # pass the url to requests, HTTP for humans
+    source_code = requests.get(url, headers=headers)
     plain_text = source_code.text
 
     content = ''
-    soup = BeautifulSoup(plain_text, "html.parser")  # pass/parse the url with bs4
+    soup = BeautifulSoup(plain_text, "html.parser")
     for items in soup.find_all("div", {"class": "s-item__info clearfix"}):
-        # work on filtering
+        # filtering
         for char in items.get_text():
             if char in string.printable:
                 content += char
+
         content += '\n\n\n'
 
     return content
 
 
 def sold_menu(products):
-    """ Scrap EBAY for sold items of the search product.
-        This changes the url to find the sold listings.
-        New URL will be fed to requests\bs4.
-        BREAK THIS UP MAYNE
-    """
+    """ Scrap EBAY for sold items of the search product. """
 
     print('\nDisplaying Sold Items For:' + ' ' + products.title() + '\n')
     sold_url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=' + products + '&_sacat=0&LH_Sold=1&_dmd=2'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    src_sold = requests.get(sold_url, headers=headers)  # pass the url to requests, HTTP for humans
+    src_sold = requests.get(sold_url, headers=headers)
     bs4_text = src_sold.text
-    # Throw findings to bs4 then display
+
     sold_soup = BeautifulSoup(bs4_text, 'html.parser')
     filtered = ''
     for sold in sold_soup.find_all("li", {"class": "s-item"}):
         print('\n' + sold.get_text())
-        # work on filtering
         for char in sold.get_text():
             if char in string.printable:
                 filtered += char
         filtered += '\n\n\n'
 
+    # throw all this in helper func()
     print(filtered)
     sg.ChangeLookAndFeel('GreenMono')
     sg.SetOptions(element_padding=(5, 0))
@@ -66,6 +93,7 @@ def sold_menu(products):
 
     window = sg.Window("Ebay Feed", default_element_size=(12, 1), auto_size_text=False,
                            auto_size_buttons=True).Layout(layout)
+
     # read values from buttons and respond accordingly
     while True:
         event, value = window.Read()
@@ -80,11 +108,10 @@ def sold_menu(products):
 def begin_form():
     """ Prompt the user what products to find """
 
-    sg.ChangeLookAndFeel('TealMono')
+    sg.ChangeLookAndFeel('LightGreen')
     layout = [
                 [sg.Text('Search Ebay!', size=(21, 1), justification='center', font=("Helvetica", 35), text_color="blue",
                  relief=sg.RELIEF_RIDGE)],
-                # change to text input field
                 [sg.Text('Enter products to search for', pad=(210, 5))],
                 [sg.InputText(focus=True, pad=(130, 5))],
                 [sg.Button('Find Products', button_color=('black', 'red'), font=("Helvetica", 15), pad=(225, 5))]
@@ -97,13 +124,17 @@ def begin_form():
             val_string = ''.join(value)
             print(val_string)
             test_menus(val_string)
-        # change to check for text input
         else:
             return
 
 
 def test_menus(product):
-    """ Display information """
+    """ First GUI page. Scraps ebay for products.
+        TODO:
+            - add links
+            - more button options
+            - format improvements
+     """
 
     output = bayscrap(product)
     # work on filtering
@@ -127,6 +158,7 @@ def test_menus(product):
 
     window = sg.Window("Ebay Feed", default_element_size=(12, 1), auto_size_text=False,
                            auto_size_buttons=True).Layout(layout)
+
     # read values from buttons and respond accordingly
     while True:
         event, value = window.Read()
